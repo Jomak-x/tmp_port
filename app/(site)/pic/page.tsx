11570bfba4c1e4ac890f3efa7f28d59afc920a66
Jomak-x <Jakob@laise.de>
2026-06-05 15:54:47 +0200

    fix pic

--- app/(site)/pic/page.tsx
+++ app/(site)/pic/page.tsx
@@ -1,15 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 import { profile } from "@/data/profile";
 
-<Image
-  src={profile.image}
-  alt="Jakob Laise"
-  width={256}
-  height={256}
-  priority
-  className="mb-8 h-44 w-44 rounded-full border-4 border-orange-400 object-cover shadow-2xl shadow-orange-950/30 sm:h-64 sm:w-64"
-/>;
-
-export default async function pic() {
-  <Image src={profile.image} alt="Jakob Laise" />;
+export default function PicPage() {
+  redirect(profile.image);
 }
